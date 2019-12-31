37f1c24d0a5676606eb5c00d428e30114b4dacc8
Jigyoomin <earth1223@sk.com>
2019-12-31 13:55:49 +0900

    bugfix

--- src/algo/Eleven.java
+++ src/algo/Eleven.java
@@ -1,43 +1,56 @@
 package algo;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class Eleven {
 
     public static void main(String[] args) throws IOException {
+        args = readFile();
+        
+        Queue<String> queue = new LinkedList<>();
+        for (String arg : args) {
+            queue.add(arg);
+        }
+        
         Eleven e = new Eleven();
-        e.start();
+        e.start(queue);
+    }
+
+
+    private static String[] readFile() throws FileNotFoundException, IOException {
+        FileReader fr = new FileReader("11-in.txt");
+        BufferedReader br = new BufferedReader(fr);
+        String l = null;
+        List<String> list = new ArrayList<>();
+        while ((l = br.readLine()) != null) {
+            list.add(l);
+        }
+        br.close();
+        
+        return list.toArray(new String[list.size()]);
     }
     
-    private BufferedReader br;
+    private void start(Queue<String> queue) throws IOException {
+        int count = Integer.parseInt(queue.poll());
     
-    private void start() throws IOException {
-
-        FileReader fr = new FileReader("11-in.txt");
-        br = new BufferedReader(fr);
-
-        try {
-            int count = Integer.parseInt(getLine());      
-    
-            for (int i = 0 ; i < count ; i++) {
-                doSolve(i + 1);
-            }
-        } catch (Exception e) {
-            
-        } finally {
-            br.close();
+        for (int i = 0 ; i < count ; i++) {
+            doSolve(i + 1, queue);
         }
     }
     
-    private void doSolve(int caseNum) throws IOException {
-        int[] question = split(getLine());
+    private void doSolve(int caseNum, Queue<String> queue) throws IOException {
+        int[] question = split(queue.poll());
         int tiles = question[0];
         int targetNum = question[1];
         
 //        System.out.println(String.format("%d %d", tiles, targetNum));
-        
         
         int lastInnerLayerSideNum = getLastInnerLayerSideNum(tiles);
         int lastInnerLayerNum = lastInnerLayerSideNum * lastInnerLayerSideNum;
@@ -46,12 +59,13 @@ public class Eleven {
             return;
         }
         
-        int currLayerNum = lastInnerLayerSideNum + 2;
+        int currLayerNum = lastInnerLayerSideNum == 0 ? lastInnerLayerSideNum + 1 : lastInnerLayerSideNum + 2;
         
         int depth = tiles - lastInnerLayerNum - 1;
         
         int min = lastInnerLayerNum + 1;
         int max = currLayerNum * currLayerNum;
+//        System.out.println(String.format("mix, max : %d, %d", min, max));
         int rightLength = targetNum - min;
         int leftLength = max - targetNum + 1;
         
@@ -60,16 +74,27 @@ public class Eleven {
             return;
         }
         
-        int totalCase = (int) Math.pow(2, depth);
-        int trueCase = 0;
+        if (tiles == max) {
+            printAnswer(caseNum, 1, 1);
+            return;
+        }
+        
+        double totalCase = Math.pow(2, depth);
+        double trueCase = 0;
         
         int i = 0;
         while (depth - rightLength >= i) {
-            trueCase += combination(depth, i++);
+            double c = combination(depth, i);
+            trueCase += c;
+//            System.out.println(String.format("%d C %d = %.0f", depth, i, c));
+            i++;
         }
         i = 0;
         while (depth - leftLength >= i) {
-            trueCase += combination(depth, i++);
+            double c = combination(depth, i);
+            trueCase += c;
+//            System.out.println(String.format("%d C %d = %.0f", depth, i, c));
+            i++;
         }
         
         printAnswer(caseNum, trueCase, totalCase);
@@ -77,21 +102,21 @@ public class Eleven {
     
     
     
-    private void printAnswer(int caseNum, int trueCase, int totalCase) {
+    private void printAnswer(int caseNum, double trueCase, double totalCase) {
         while (trueCase > 0 && trueCase % 2 == 0) {
             trueCase /= 2;
             totalCase /= 2;
         }
-        System.out.println(String.format("#%d %d / %d", caseNum, trueCase, totalCase));
+        System.out.println(String.format("#%d %.0f / %.0f", caseNum, trueCase, totalCase));
     }
 
-    private int combination(int total, int num) {
+    private double combination(int total, int num) {
         if (num == 0) {
             return 1;
         }
         
         int big = total - num > num ? total-num : num;
-        int comb = 1;
+        double comb = 1;
         for (int i = total ; i > big ; i--) {
             comb *= i;
         }
@@ -112,17 +137,6 @@ public class Eleven {
         return innerLayer;
     }
     
-    private boolean isInInnerLayer(int lastInnerLayerSideNum, int targetNum)  {
-        if ( lastInnerLayerSideNum * lastInnerLayerSideNum + 1 >= targetNum) {
-            return true;
-        }
-        
-        return false;
-    }
-    
-    private String getLine() throws IOException {
-        return br.readLine();
-    }
     
     private int[] split(String str) {
         String[] split = str.split(" ");
